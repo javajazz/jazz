3ace582eb9dbc0d7004ffa6bfbc62b9c004b6bb9
Julian Fleischer <julian.fleischer@fu-berlin.de>
2013-12-24 14:57:38 +0100

    Added package-infos

---
+++ src/main/java/jazz/pictures/immutable/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * This package contains a set of predefined <i>mutable</i> pictures, such as
+ * circles, rectangles, polygons and bitmaps.
+ * 
+ * All classes in this package are strictly immutable and all their setter
+ * functions will create a new object instead of altering <i>this</i> object.
+ */
+package jazz.pictures.immutable;
+
