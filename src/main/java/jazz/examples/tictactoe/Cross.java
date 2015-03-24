0a37056f8bcb4e7104e500f895604756dd86817e
Julian <julfleischer@paypal.com>
2015-03-24 15:34:58 +0100

    TicTacToe

---
+++ src/main/java/jazz/examples/tictactoe/Cross.java
@@ -0,0 +1,27 @@
+package jazz.examples.tictactoe;
+
+import jazz.Vector;
+import jazz.pictures.DelegatingPicture;
+import jazz.pictures.mutable.Polygon;
+
+public class Cross extends DelegatingPicture<Cross> {
+
+	public Cross(double size) {
+		super(new Polygon(
+				new Vector(-0.5 * size, -0.25 * size),
+				new Vector(-0.25 * size, -0.5 * size),
+				new Vector(0, -0.25 * size),
+				new Vector(0.25 * size, -0.5 * size),
+				new Vector(0.5 * size, -0.25 * size),
+
+				new Vector(0.25 * size, 0),
+
+				new Vector(0.5 * size, 0.25 * size),
+				new Vector(0.25 * size, 0.5 * size),
+				new Vector(0, 0.25 * size),
+				new Vector(-0.25 * size, 0.5 * size),
+				new Vector(-0.5 * size, 0.25 * size),
+
+				new Vector(-0.25 * size, 0)));
+	}
+}
---
+++ src/main/java/jazz/examples/tictactoe/TicTacToe.java
@@ -0,0 +1,10 @@
+package jazz.examples.tictactoe;
+
+import jazz.Jazz;
+
+public class TicTacToe {
+
+	public static void main(String... args) {
+		Jazz.play("Tic Tac Toe", 600, 600, new GameBoard());
+	}
+}
